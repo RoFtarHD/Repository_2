bbeaadcfefcdff64e18f6e2293409a3c634a331d
DenissK20 <deniska.riga@gmail.com>
2017-11-17 17:06:21 +0200

    r1

---
+++ 11.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+$app = new \atk4\ui\App('Загрузка наших мозгов...');
+$app->initLayout('Centered');
+$button = $app->add('Button');
+$button->set('Follower');
+$button->icon = 'comment';
+$button->link ('https://www.youtube.com');
+$button = $app->add('Button');
+$button->set('Registration');
+$button->icon = 'youtube';
+$button->link ('https://www.e-klase.lv');
+$button = $app->add('Button');
+$button->set('Android site');
+$button->icon = 'android';
+$button->link ('https://www.android.com');
+$button = $app->add('Button');
+$button->set('My chess site');
+$button->icon = 'delicious';
+$button->link ('http://deniskariga.wixsite.com/chess-lessons-free');
